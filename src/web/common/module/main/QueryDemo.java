faaa3a4197650b7ca1ce7147c7cd919b9f81726f
Kanglt <970458348@qq.com>
2016-11-07 13:35:46 +0800

    测试了数据库操作

--- src/web/common/module/main/QueryDemo.java
+++ src/web/common/module/main/QueryDemo.java
@@ -14,20 +14,14 @@ import java.sql.Statement;
 import lyu.klt.frame.database.ConnectionAgent;
 public class QueryDemo {
 
-	public static void main(String[] args){
-//		String JDriver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-//		String url="jdbc:sqlserver://10.10.76.20:1433;DatabaseName=GraduationDesign_KLT_DB";
-//		String user="sa";
-//		String password="119406";
-		
+	public static void main(String[] args){	
 		try{
-		//	Class.forName(JDriver);// 动态导入数据库的驱动
 			Connection conn=ConnectionAgent.getInstance().getConnection();// 获取数据库链接 
-			String query="SELECT * FROM user";// 创造SQL语句 
+			String query="SELECT * FROM [user]";// 创造SQL语句 
 			Statement stmt=conn.createStatement();// 执行SQL语句
 			ResultSet rs=stmt.executeQuery(query);
 			while(rs.next()){
-				System.out.println(rs.getString("user_name")+":"+rs.getString(2));
+				System.out.println(rs.getString("userName")+":"+rs.getString(2));
 				//密码字段的编号从1开始，密码排第二位
 			}
 			System.out.println("查询数据成功");
